3bec23b40b489f76a85babe5dff50f1295222907
ATPJ <amirali.porhonar@gmail.com>
2023-03-17 13:18:21 +0330

    Make User model and UserManager

---
+++ app/core/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, password=None, **extra_fields):
+        """ Create new User """
+        if not username:
+            raise ValueError("User must have a username")
+
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, username, password, **extra_fields):
+        """ Create Superuser """
+        user = self.create_user(username=username, password=password,
+                                **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "username"
